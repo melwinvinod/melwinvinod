7248ff43149d32a6f6a8a20bc6b558e27972508e
melwinvinod <54060915+melwinvinod@users.noreply.github.com>
2021-11-24 18:04:35 +0530

    This code works

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
 from fastapi import FastAPI
 import json
-from jsonpath_ng import jsonpath, parse
 
 app =  FastAPI()
-data=[
-	{
-		"random": 74,
-		"firstname": "Rayna",
-		"lastname": "Standing"
-	},
-	{
-		"random": 64,
-		"firstname": "Karolina",
-		"lastname": "Lauraine"
-	},
-	{
-		"random": 6,
-		"firstname": "Joeann",
-		"lastname": "Ingra"
-	},
-	{
-		"random": 3,
-		"firstname": "Jemie",
-		"lastname": "Carey"
-	}
-]
+data=[{
+  "name": "Elon Musk",
+  "email": "elon@spacex.com",
+  "image": "link to image"
+},
+{
+  "name": "Jeff Bezos",
+  "email": "jeff@blueorigin.com",
+  "image": "link to image"
+}]
 
 @app.get("/")
 def func1():
 	return data
 
 @app.get("/{random1}")
-def return_anme(random1: int):
-	json_data = json.loads(data)
-	jsonpath_expr = parse('data[0]')
-	return jsonpath_expr
+def return_data(random1: str):
+	for i in data:
+		if i['name'] == random1:
+			return i
